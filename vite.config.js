ba5444c60331a46bf14304dd5dac1cb81674f359
JenniferMiao <JenniferMiao@easontech.com.tw>
2026-04-01 15:10:08 +0800

    configure github pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/nasEc_secnd/',
   plugins: [vue(), tailwindcss()],
 });
